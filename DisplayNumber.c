12cc8f59e83d99e59ab8cbcfdf646581f335646a
Nikhil Jadhav <84830137+jn935@users.noreply.github.com>
2021-08-17 21:18:03 +0530

    Create DisplayNumber.c
    Accept a number with 1 or 2 digit from user and display it in words.

---
+++ DisplayNumber.c
@@ -0,0 +1,344 @@
+ #include <stdio.h>
+void main()
+{
+   int cdigit;
+
+   printf("Input Digit(0-99) : ");
+   scanf("%d",&cdigit);
+   switch(cdigit)
+   {
+	 case 0:
+	       printf("Zero\n");
+	       break;
+ 
+	 case 1:
+	       printf("one\n");
+	       break;
+	case 2:
+	       printf("Two\n");
+	       break;
+	case 3:
+	       printf("Three\n");
+	       break;
+	case 4:
+	       printf("Four\n");
+	       break;
+	case 5:
+	       printf("Five\n");
+	       break;
+	case 6:
+	       printf("Six\n");
+	       break;
+	case 7:
+	       printf("Seven\n");
+	       break;
+	case 8:
+	       printf("Eight\n");
+	       break;
+	case 9:
+	       printf("Nine\n");
+	       break;
+
+   
+	 case 10:
+	       printf("Ten\n");
+	       break;
+ 
+	 case 11:
+	       printf("eleone\n");
+	       break;
+	case 12:
+	       printf("Twelve\n");
+	       break;
+	case 13:
+	       printf("Thirteen \n");
+	       break;
+	case 14:
+	       printf("Fourteen\n");
+	       break;
+	case 15:
+	       printf("Fifteen\n");
+	       break;
+	case 16:
+	       printf("Sixteen\n");
+	       break;
+	case 17:
+	       printf("Seventeen\n");
+	       break;
+	case 18:
+	       printf("Eighteen\n");
+	       break;
+	case 19:
+	       printf("Nineteen\n");
+	       break;
+	
+	 case 20:
+	       printf("Twenty\n");
+	       break;
+ 
+	 case 21:
+	       printf("Twenty one\n");
+	       break;
+	case 22:
+	       printf("Twenty Two\n");
+	       break;
+	case 23:
+	       printf("Twenty Three\n");
+	       break;
+	case 24:
+	       printf("Twenty Four\n");
+	       break;
+	case 25:
+	       printf("Twenty Five\n");
+	       break;
+	case 26:
+	       printf("Twenty Six\n");
+	       break;
+	case 27:
+	       printf("Twenty Seven\n");
+	       break;
+	case 28:
+	       printf("Twenty Eight\n");
+	       break;
+	case 29:
+	       printf("Twenty Nine\n");
+	       break;
+	
+	 case 30:
+	       printf("Thirty\n");
+	       break;
+ 
+	 case 31:
+	       printf("Thirtyone\n");
+	       break;
+	case 32:
+	       printf("Thirty Two\n");
+	       break;
+	case 33:
+	       printf("Thirty Three\n");
+	       break;
+	case 34:
+	       printf("Thirty Four\n");
+	       break;
+	case 35:
+	       printf("Thirty Five\n");
+	       break;
+	case 36:
+	       printf("Thirty Six\n");
+	       break;
+	case 37:
+	       printf("Thirty Seven\n");
+	       break;
+	case 38:
+	       printf("Thirty Eight\n");
+	       break;
+	case 39:
+	       printf("Thirty Nine\n");
+	       break;
+	
+   
+	 case 40:
+	       printf("Fourty\n");
+	       break;
+ 
+	 case 41:
+	       printf("Fourty one\n");
+	       break;
+	case 42:
+	       printf("Fourty Two\n");
+	       break;
+	case 43:
+	       printf("Fourty Three\n");
+	       break;
+	case 44:
+	       printf("Fourty Four\n");
+	       break;
+	case 45:
+	       printf("Fourty Five\n");
+	       break;
+	case 46:
+	       printf("Fourty Six\n");
+	       break;
+	case 47:
+	       printf("Fourty Seven\n");
+	       break;
+	case 48:
+	       printf("Fourty Eight\n");
+	       break;
+	case 49:
+	       printf("Fourty Nine\n");
+	       break;
+	
+	 case 50:
+	       printf("Fifty\n");
+	       break;
+ 
+	 case 51:
+	       printf("Fifty one\n");
+	       break;
+	case 52:
+	       printf("Fifty Two\n");
+	       break;
+	case 53:
+	       printf("Fifty Three\n");
+	       break;
+	case 54:
+	       printf("Fifty Four\n");
+	       break;
+	case 55:
+	       printf("Five\n");
+	       break;
+	case 56:
+	       printf("Fifty Six\n");
+	       break;
+	case 57:
+	       printf("Fifty Seven\n");
+	       break;
+	case 58:
+	       printf("Fifty Eight\n");
+	       break;
+	case 59:
+	       printf("Fifty Nine\n");
+	       break;
+	
+	 case 60:
+	       printf("Sixty\n");
+	       break;
+ 
+	 case 61:
+	       printf("Sixtyone\n");
+	       break;
+	case 62:
+	       printf("Sixty Two\n");
+	       break;
+	case 63:
+	       printf("Sixty Three\n");
+	       break;
+	case 64:
+	       printf("Sixty Four\n");
+	       break;
+	case 65:
+	       printf("Sixty Five\n");
+	       break;
+	case 66:
+	       printf("Sixty Six\n");
+	       break;
+	case 67:
+	       printf("Sixty Seven\n");
+	       break;
+	case 68:
+	       printf("Sixty Eight\n");
+	       break;
+	case 69:
+	       printf("Sixty Nine\n");
+	       break;
+	
+	 case 70:
+	       printf("Seventy\n");
+	       break;
+ 
+	 case 71:
+	       printf("Seventy one\n");
+	       break;
+	case 72:
+	       printf("Seventy Two\n");
+	       break;
+	case 73:
+	       printf("Seventy Three\n");
+	       break;
+	case 74:
+	       printf("Seventy Four\n");
+	       break;
+	case 75:
+	       printf("Seventy Five\n");
+	       break;
+	case 76:
+	       printf("Seventy Six\n");
+	       break;
+	case 77:
+	       printf("Seventy Seven\n");
+	       break;
+	case 78:
+	       printf("Seventy Eight\n");
+	       break;
+	case 79:
+	       printf("Seventy Nine\n");
+	       break;
+	
+	 case 80:
+	       printf("Eighty\n");
+	       break;
+ 
+	 case 81:
+	       printf("Eighty one\n");
+	       break;
+	case 82:
+	       printf("Eighty Two\n");
+	       break;
+	case 83:
+	       printf("Eighty Three\n");
+	       break;
+	case 84:
+	       printf("Eighty Four\n");
+	       break;
+	case 85:
+	       printf("Eighty Five\n");
+	       break;
+	case 86:
+	       printf("Eighty Six\n");
+	       break;
+	case 87:
+	       printf("Eighty Seven\n");
+	       break;
+	case 88:
+	       printf("Eighty Eight\n");
+	       break;
+	case 89:
+	       printf("Eighty Nine\n");
+	       break;
+	
+   
+	 case 90:
+	       printf("Ninety\n");
+	       break;
+ 
+	 case 91:
+	       printf("Ninety one\n");
+	       break;
+	case 92:
+	       printf("Ninety Two\n");
+	       break;
+	case 93:
+	       printf("Ninety Three\n");
+	       break;
+	case 94:
+	       printf("Ninety Four\n");
+	       break;
+	case 95:
+	       printf("Ninety Five\n");
+	       break;
+	case 96:
+	       printf("Ninety Six\n");
+	       break;
+	case 97:
+	       printf("Ninety Seven\n");
+	       break;
+	case 98:
+	       printf("Ninety Eight\n");
+	       break;
+	case 99:
+	       printf("Ninety Nine\n");
+	       break;
+	
+	   
+	 case 100:
+	       printf("Zero\n");
+	       break;
+ 
+	 
+	default:
+	       printf("invalid digit. \nPlease try again ....\n");
+	       break;
+	   }
+	   
+		   }
